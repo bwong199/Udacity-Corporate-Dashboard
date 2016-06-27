45e815ee829a68fa1598165fb3e9a50682e3bc8c
bwong199 <bwong199@my.bcit.ca>
2016-06-26 19:07:00 -0600

    Concat into a single file

--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,13 @@ gulp.task('scripts', function(){
 		.pipe(gulp.dest('./build/scripts/'))
 });
 
+// gulp.task('scripts', function() {
+//   return gulp.src(['./src/scripts/*.js'])
+//     .pipe(concat('all.js'))
+//     .pipe(gulp.dest('./build/scripts/'));
+// });
+
+
 gulp.task('styles', function() {
   gulp.src(['./src/styles/*.css'])
     .pipe(concat('styles.css'))
